5bdcb0f001cf20fdb663f605fe73a49cf1571a3a
AlexSobenin <sobeninalex@yandex.ru>
2023-03-12 17:04:46 +0500

    add SeleniumTests_2.0

--- TestIMT/src/test/java/SeleniumTests.java
+++ TestIMT/src/test/java/SeleniumTests.java
@@ -1,41 +1,54 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTests {
+    private WebDriver driver;
 
-    private final WebDriver driver = new ChromeDriver();
-    public void setProperty() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+    @Before
+    public void setUp() {
+//        TODO: пердаем путь к веб-драйверу;
+//        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+//        TODO: так как после обновления хрома все сломалось теперь делаем так;
+//        TODO: создаем переменную класса ChromeOption и добавляем аргумент;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+//        TODO: передаем переменную options в параметры переменной driver класса ChromeDriver;
+        driver = new ChromeDriver(options);
+
+        driver.navigate().to("https://lm.skillbox.cc/qa_tester/module01/");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 
     @Test
     public void testHello() {
-        setProperty();
-
-        driver.navigate().to("https://lm.skillbox.cc/qa_tester/module01/");
         driver.findElement(By.name("name")).sendKeys("Алексиос");
         driver.findElement(By.className("custom-form__button")).click();
 
         var actualResult = driver.findElement(By.className("start-screen__res")).getText();
         var expectedResult = "Привет, Алексиос!";
-        Assert.assertEquals(expectedResult, actualResult);
 
-        driver.quit();
+        Assert.assertEquals(expectedResult, actualResult);
     }
+
     @Test
     public void testFieldNameIsNull() {
-        setProperty();
-
-        driver.navigate().to("https://lm.skillbox.cc/qa_tester/module01/");
         driver.findElement(By.className("custom-form__button")).click();
 
         var actualResult = driver.findElement(By.className("start-screen__res")).getText();
         var expectedResult = "Привет, !";
-        Assert.assertEquals(expectedResult, actualResult);
 
-        driver.quit();
+        Assert.assertEquals(expectedResult, actualResult);
     }
 }
--- TestIMT/src/test/java/SeleniumTests_1.java
+++ TestIMT/src/test/java/SeleniumTests_1.java
@@ -1,4 +1,3 @@
-import org.checkerframework.checker.units.qual.A;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -6,33 +5,37 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumTests_1 {
-    private final WebDriver driver = new FirefoxDriver();
+
+    private WebDriver driver;
 
     @Before
     public void setUp() {
+//        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.navigate().to("https://lm.skillbox.cc/qa_tester/module02/homework1/");
     }
+
     @After
-    public void exit() {
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
     public void testOpenPage() {
-
         var actualResult = driver.findElement(By.className("start-screen__text")).getText();
         var expectedResult = "Введите свои данные";
 
-        Assert.assertEquals(expectedResult, actualResult);
-
+        Assert.assertEquals("Страница не загрузилась", expectedResult, actualResult);
     }
 
     @Test
-    public  void testCorrectAllData() {
-
+    public void testCorrectAllData() {
         driver.findElement(By.name("name")).sendKeys("Иванов Иван Иванович");
         driver.findElement(By.name("email")).sendKeys("ivan.ivanov@yandex.ru");
         driver.findElement(By.name("phone")).sendKeys("+79995554433");
@@ -44,13 +47,10 @@ public class SeleniumTests_1 {
                 "Наш сотрудник свяжется с вами по телефону: +79995554433.";
 
         Assert.assertEquals(expectedResult, actualResult);
-
     }
 
     @Test
     public void testDataNoPhone() {
-
-
         driver.findElement(By.name("name")).sendKeys("Иванов Иван Иванович");
         driver.findElement(By.name("email")).sendKeys("ivan.ivanov@yandex.ru");
         driver.findElement(By.className("custom-form__button")).click();
@@ -59,14 +59,11 @@ public class SeleniumTests_1 {
         var expectedResult = "Здравствуйте, Иванов Иван Иванович.\n" +
                 "На вашу почту (ivan.ivanov@yandex.ru) отправлено письмо.\n";
 
-        try {Assert.assertEquals(expectedResult, actualResult);}
-        finally {driver.quit();}
+        Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testDataNoEmail() {
-
-
         driver.findElement(By.name("name")).sendKeys("Иванов Иван Иванович");
         driver.findElement(By.name("phone")).sendKeys("+79995554433");
         driver.findElement(By.className("custom-form__button")).click();
@@ -75,35 +72,28 @@ public class SeleniumTests_1 {
         var expectedResult = "Здравствуйте, Иванов Иван Иванович.\n" +
                 "Наш сотрудник свяжется с вами по телефону: +79995554433.";
 
-        try {Assert.assertEquals(expectedResult, actualResult);}
-        finally {driver.quit();}
+        Assert.assertEquals("Неверный текст приветствия", expectedResult, actualResult);
     }
 
     @Test
     public void testDataNoEmailNoPhone() {
-
-
         driver.findElement(By.name("name")).sendKeys("Иванов Иван Иванович");
         driver.findElement(By.className("custom-form__button")).click();
 
         var actualResult = driver.findElement(By.className("start-screen__res")).getText();
         var expectedResult = "Здравствуйте, Иванов Иван Иванович.";
 
-        try {Assert.assertEquals(expectedResult, actualResult);}
-        finally {driver.quit();}
+        Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testNoData() {
-
-
         driver.findElement(By.className("custom-form__button")).click();
 
         var actualResult = driver.findElement(By.className("start-screen__res")).getText();
         var expectedResult = "Пожалуйста, введите свои данные";
 
-        try {Assert.assertEquals(expectedResult, actualResult);}
-        finally {driver.quit();}
+        Assert.assertEquals(expectedResult, actualResult);
     }
 
 }
