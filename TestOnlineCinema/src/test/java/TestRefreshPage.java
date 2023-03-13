1623491f78bcc000047886682edfe6d8c613a8d2
AlexSobenin <sobeninalex@yandex.ru>
2023-03-13 22:29:28 +0500

    upgrade SeleniumTests_3.2

--- TestOnlineCinema/src/test/java/TestRefreshPage.java
+++ TestOnlineCinema/src/test/java/TestRefreshPage.java
@@ -17,6 +17,11 @@ public class TestRefreshPage {
         driver.navigate().to("https://lm.skillbox.cc/qa_tester/module07/practice3/");
     }
 
+    @Before
+    public void sleep() throws InterruptedException {
+        Thread.sleep(2500);
+    }
+
     @After
     public void tearDown() {
         driver.navigate().refresh();
@@ -28,31 +33,42 @@ public class TestRefreshPage {
     }
 
     @Test
-    public void testCorrectFieldOnePage() {
+    public void testCorrectFieldOnePage() throws InterruptedException {
         String films = "Брат, Брат-2";
         String serials = "Игра престолов, Бестыжие, Шерлок";
 
         driver.findElement(By.id("films")).sendKeys(films);
+        Thread.sleep(1500);
         driver.findElement(By.id("serials")).sendKeys(serials);
+        Thread.sleep(1500);
         driver.findElement(By.className("section__button")).click();
         driver.findElement(By.id("two")).click();
+        Thread.sleep(1500);
         driver.findElement(By.className("section__button")).click();
+        Thread.sleep(1500);
         driver.findElement(By.id("ok")).click();
+
 
         var actualFilms = driver.findElement(By.id("best_films")).getText();
         var actualSerials = driver.findElement(By.id("best_serials")).getText();
 
         Assert.assertEquals(films, actualFilms);
         Assert.assertEquals(serials, actualSerials);
+        Thread.sleep(1500);
     }
 
     @Test
-    public void testCorrectFieldTwoPage() {
+    public void testCorrectFieldTwoPage() throws InterruptedException {
         driver.findElement(By.id("two")).click();
+        Thread.sleep(1500);
         driver.findElement(By.xpath("//*[@id=\"page_2\"]/div[1]/div[2]/div[2]/label")).click();
+        Thread.sleep(1500);
         driver.findElement(By.xpath("//*[@id=\"page_2\"]/div[1]/div[2]/div[4]/label")).click();
+        Thread.sleep(1500);
         driver.findElement(By.cssSelector("#page_2 > div:nth-child(2) > div.module__inputs > div:nth-child(3) > label > span")).click();
+        Thread.sleep(1500);
         driver.findElement(By.xpath("//*[@id=\"save\"]")).click();
+        Thread.sleep(1500);
         driver.findElement(By.xpath("//*[@id=\"ok\"]")).click();
 
         List<WebElement> list = driver.findElements(By.className("result__content"));
@@ -64,12 +80,15 @@ public class TestRefreshPage {
 
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedSize, actualSeize);
+        Thread.sleep(1500);
     }
 
     @Test
-    public void testAllFieldsIsNull() {
+    public void testAllFieldsIsNull() throws InterruptedException {
         driver.findElement(By.xpath("//*[@id=\"two\"]")).click();
+        Thread.sleep(1500);
         driver.findElement(By.xpath("//*[@id=\"save\"]")).click();
+        Thread.sleep(1500);
         driver.findElement(By.xpath("//*[@id=\"ok\"]")).click();
 
         List<WebElement> list = driver.findElements(By.className("result__content"));
@@ -77,5 +96,6 @@ public class TestRefreshPage {
         String expectedTestFavoriteFilms = "";
 
         Assert.assertEquals(expectedTestFavoriteFilms, actualTextFavoriteFilms);
+        Thread.sleep(1500);
     }
 }
